640ab5c66b86dc98ca7c70861771554077ce0840
Michael Obernhumer <michael.obernhumer@htl-braunau.at>
2022-06-21 10:40:20 +0200

    aktualisieren des offline statues

---
+++ test.py
@@ -0,0 +1,15 @@
+path="C:\\Users\\Michael Obernhumer\\Documents\\prgoramms_selfmade\\Skripts\\file-finder\\l1dir1\\l2dir2\\l2dir2file1.md"
+
+import hashlib
+
+file = path # Location of the file (can be set a different way)
+BLOCK_SIZE = 65536 # The size of each read from the file
+
+file_hash = hashlib.sha256() # Create the hash object, can use something other than `.sha256()` if you wish
+with open(file, 'rb') as f: # Open the file to read it's bytes
+    fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+    while len(fb) > 0: # While there is still data being read from the file
+        file_hash.update(fb) # Update the hash
+        fb = f.read(BLOCK_SIZE) # Read the next block from the file
+
+print (file_hash.hexdigest()) # Get the hexadecimal digest of the hash
